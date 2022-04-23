b7a924e1e0e88699e565ec4f353b7c781ddaa2cb
Jason_Shu <shuyue524@gmail.com>
2022-04-23 17:07:16 +0800

    模拟记录请求时间

--- server.ts
+++ server.ts
@@ -3,13 +3,23 @@ import Koa from "koa";
 const app = new Koa();
 
 app.use(async (ctx, next) => {
-  ctx.body = "hello world!";
   await next();
-  ctx.body += " 最后执行的呀！";
+  const time = ctx.response.get("X-Response-Time");
+  console.log(`${ctx.url}: ${time}`);
 });
 
-app.use((ctx, next) => {
-  ctx.body += " 中间执行de!";
+app.use(async (ctx, next) => {
+  const startTime = Date.now(); // 记录开始时间
+  await next();
+  const time = Date.now() - startTime;
+  ctx.set("X-Response-Time", `${time}ms`);
+});
+
+app.use(async (ctx, next) => {
+  await next();
+  for (let i = 0; i < 1000; i++) {
+    ctx.body += "Hello World!\n";
+  }
 });
 
 app.listen(3000);
