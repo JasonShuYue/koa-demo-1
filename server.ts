29ed91e3ed48415ff2370e3d9e35deae5ef09b9d
Jason_Shu <shuyue524@gmail.com>
2022-04-23 16:47:45 +0800

    尝试u模型,使用await next()

--- server.ts
+++ server.ts
@@ -2,8 +2,14 @@ import Koa from "koa";
 
 const app = new Koa();
 
-app.use((ctx) => {
+app.use(async (ctx, next) => {
   ctx.body = "hello world!";
+  await next();
+  ctx.body += " 最后执行的呀！";
+});
+
+app.use((ctx, next) => {
+  ctx.body += " 中间执行de!";
 });
 
 app.listen(3000);
